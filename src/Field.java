e37198952e90969410cae658bc296b383456c122
Yamui <mbrode211@gmail.com>
2017-09-07 12:40:23 +0200

    FieldDefinition

--- src/Field.java
+++ src/Field.java
@@ -1,5 +1,39 @@
 
 public class Field
 {
+    private boolean	isHome;
+    private Piece	occupied;
+    private final int 	NUMBER;
+    
+    public Field(boolean isHome, int number)
+    {
+	this.isHome = isHome;
+	this.occupied = null;
+	this.NUMBER = number;
+    }
 
+    public boolean isHome()
+    {
+        return isHome;
+    }
+
+    public void setHome(boolean isHome)
+    {
+        this.isHome = isHome;
+    }
+
+    public Piece getOccupied()
+    {
+        return occupied;
+    }
+
+    public void setOccupied(Piece occupied)
+    {
+        this.occupied = occupied;
+    }
+
+    public int getNUMBER()
+    {
+        return NUMBER;
+    }
 }
